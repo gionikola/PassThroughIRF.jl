f11af62090e6a3ef1d4633bbb5061b0f4cec6e05
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-09-23 02:02:13 +0400

    Fix basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "PassThroughIRF.jl" begin
     
-    @test allpaths(1, 1)[1][1] == 1
-    @test allpaths(2, 2)[1][1] == 2
+    @test PassThroughIRF.allpaths(1, 1)[1][1] == 1
+    @test PassThroughIRF.allpaths(2, 2)[1][2] == 2
     
 end
