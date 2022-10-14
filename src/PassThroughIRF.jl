8bf65f4a09fe6795fe2aab0c336c445057e18fc4
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-10-14 13:59:59 -0700

    Add basic dependencies

--- src/PassThroughIRF.jl
+++ src/PassThroughIRF.jl
@@ -1,9 +1,14 @@
 module PassThroughIRF
 
+    using LinearAlgebra
+    using Statistics 
+    using Random
+
     include("paths.jl")
     include("path_intensity.jl")
     include("irf.jl")
     include("ptirf.jl")
+    include("var.jl")
 
     export irf, ptirf 
 
