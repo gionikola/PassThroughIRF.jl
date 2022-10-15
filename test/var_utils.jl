73d7743c2bbf10542123c486a8c13ce424addd90
gionikola <gnikolai@uoregon.edu>
2022-10-15 14:00:16 -0700

    Add to test script for var estim

--- test/var_utils.jl
+++ test/var_utils.jl
@@ -1,8 +1,20 @@
 Y = rand(20, 5)
-Σ = rand(5,5)
-Σ = Σ * Σ'
+Σu = rand(5,5)
+Σu = Σu * Σu'
 lags = 3
-priormean = zeros(size(Y)[2]^2 * lags + size(Y)[2])
-priorvar = I(length(priormean)) * 1000.0 |> Matrix
+αstar = zeros(size(Y)[2]^2 * lags + size(Y)[2])
+V = I(1 + size(Y)[2] * lags) * 1000.0 |> Matrix 
 
-α = drawlagcoefficients(Y, Σ, priormean, priorvar, lags = lags)
+α, αbar = drawlagcoefficients(Y, Σu, αstar, V, lags = lags)
+
+Sstar = I(size(Y)[2]) * 1.0 |> Matrix 
+n = size(Y)[2] + 1
+
+Σu = drawerrormatrix(Y, lags, αstar, αbar, V, Sstar, n)
+
+obj1, obj2 =  drawparameters(Y, Σu, lags, αstar, V, Sstar, n)
+
+N = 5
+burnin = 2
+
+αlist, Σulist = estimatevar(Y, N, burnin, lags, αstar, V, Sstar, n)
