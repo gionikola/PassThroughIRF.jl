197d7d1aff04ac60d2bcab1533c2727b68ee3cac
gionikola <gnikolai@uoregon.edu>
2022-10-15 13:56:28 -0700

    Create VAR estimator

---
+++ src/var.jl
@@ -0,0 +1,20 @@
+"""
+"""
+function estimatevar(Y::Matrix{Float64}, N::Int64, burnin::Int64, lags::Int64, αstar::Vector{Float64}, V::Matrix{Float64}, Sstar::Matrix{Float64}, n::Int64)
+
+    αlist = Matrix{Float64}[] 
+    Σulist = Matrix{Float64}[] 
+
+    Σu = rand(size(Y)[2], size(Y)[2])
+
+    for i in 1:N
+        println("Iteration: $i")
+        α, Σu = drawparameters(Y, Σu, lags, αstar, V, Sstar, n)
+        if i > burnin 
+            push!(αlist, α)
+            push!(Σulist, Σu)
+        end 
+    end 
+
+    return αlist, Σulist 
+end 
