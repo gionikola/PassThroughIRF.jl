83d624917a56f978d9ab66e4bbf7f07dddc18019
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-10-15 17:12:28 -0700

    Minor changes

--- src/simulate_var.jl
+++ src/simulate_var.jl
@@ -2,21 +2,23 @@
 """
 function simulatevar(T::Int64, numvar::Int64, lags::Int64, coefficients::Matrix{Float64}, covariance::Matrix{Float64})
 
-    interceptvec = zeros(numvar*lags)
+    M = numvar * lags 
+
+    interceptvec = zeros(M)
     interceptvec[1:numvar] = coefficients[:,1]
 
-    companionmat = zeros(numvar*lags, numvar*lags)
+    companionmat = zeros(M, M)
     companionmat[1:numvar, :] = coefficients[:, 2:end] 
     companionmat[(numvar+1):end,1:(numvar*(lags-1))] = I(numvar*(lags-1)) * 1.0
 
-    covmat = zeros(numvar*lags, numvar*lags)
+    covmat = zeros(M, M)
     covmat[1:numvar, 1:numvar] = covariance 
 
-    datamat = zeros(T, numvar*lags)
-    datamat[1,:] = inv(I(numvar*lags) - companionmat) * interceptvec 
+    datamat = zeros(T, M)
+    datamat[1,:] = inv(I(M) - companionmat) * interceptvec 
 
     for t in 2:T 
-        datamat[t,:] = interceptvec + companionmat * vec(datamat[t-1,:]) + multinormal(zeros(numvar*lags), covmat)
+        datamat[t,:] = interceptvec + companionmat * vec(datamat[t-1,:]) + multinormal(zeros(M), covmat)
     end 
 
     return datamat[:,1:numvar] 
--- test/simulate_var.jl
+++ test/simulate_var.jl
@@ -1,4 +1,4 @@
-T = 10000
+T = 500
 numvar = 2
 lags = 2
 coefficients = [0.0 0.5 0.01 0.1 0.0
@@ -11,7 +11,14 @@ V = I(1 + size(data)[2] * lags) * 1000.0 |> Matrix
 Sstar = I(size(data)[2]) * 1.0 |> Matrix
 n = size(data)[2] + 1
 
-N = 1000
-burnin = 100
+N = 5000
+burnin = 1000
 
 αlist, Σulist = estimatevar(data, N, burnin, lags, vec(coefficients), V, Sstar, n)
+
+testvec = zeros(N - burnin)
+for i in 1:(N-burnin)
+    testvec[i] = αlist[i][1,2]
+end 
+
+plot(testvec)
