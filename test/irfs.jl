0fd2b5f88e3e0811ad3b45d2ee5e83a2048e2ba2
gionikola <gnikolai@uoregon.edu>
2022-10-16 15:37:58 -0700

    Add some test code for IRF and PTIRF distrs

--- test/irfs.jl
+++ test/irfs.jl
@@ -1,10 +1,13 @@
 using PassThroughIRF
+using LinearAlgebra
+using Statistics
+using Plots
 
-T = 500
+T = 200
 numvar = 2
-lags = 2
-coefficients = [0.0 0.5 0.01 0.1 0.0
-    0.0 0.02 0.4 0.0 -0.1]
+lags = 1
+coefficients = [0.0 0.5 0.01
+    0.0 0.02 0.4]
 covariance = [1.0 0.2; 0.2 1.0]
 
 data = simulatevar(T, numvar, lags, coefficients, covariance)
@@ -22,15 +25,15 @@ A = mean(αlist)[:,2:end]
 Σ = mean(Σulist)
 Q = PassThroughIRF.rotationmat(Σ)
 
-lagcoefmats = []
+lagcoefmats = Matrix{Float64}[]
 push!(lagcoefmats, A[1:2,1:2])
-push!(lagcoefmats, A[1:2,3:4])
+#push!(lagcoefmats, A[1:2,3:4])
 
 shockcoefmat = Q
 
 periods = 10
 shock = 2
-medium = 2
+medium = 1
 response = 2
 
 res1 = irf(response, shock, periods, lagcoefmats, shockcoefmat)
@@ -38,3 +41,51 @@ res2 = ptirf(response, medium, shock, periods, lagcoefmats, shockcoefmat)
 
 plot(res1)
 plot!(res2) 
+
+###############
+###############
+###############
+periods = 10
+shock = 1
+medium = 2
+response = 1
+
+Qlist = Matrix{Float64}[] 
+irflist = Vector{Float64}[] 
+ptirflist = Vector{Float64}[] 
+for i in 1:length(αlist)
+    println(i)
+    Q = PassThroughIRF.rotationmat(Σulist[i])
+    push!(Qlist, Q)
+    shockcoefmat = Q 
+    lagcoefmats = Matrix{Float64}[]
+    push!(lagcoefmats, αlist[i][:,2:end])
+    res1 = irf(response, shock, periods, lagcoefmats, shockcoefmat)
+    res2 = ptirf(response, medium, shock, periods, lagcoefmats, shockcoefmat)
+    push!(irflist, res1)
+    push!(ptirflist, res2)
+end 
+
+irfdistr = zeros(periods-1, 3)
+for i in 1:(periods-1)
+    responses = zeros(length(αlist))
+    for j in 1:length(αlist)
+        responses[j] = irflist[j][i]
+    end  
+    irfdistr[i,1] = mean(responses) 
+    irfdistr[i,2] = quantile(responses, 0.1)
+    irfdistr[i,3] = quantile(responses, 0.9)
+end 
+plot(irfdistr) 
+
+irfdistr = zeros(periods-1, 3)
+for i in 1:(periods-1)
+    responses = zeros(length(αlist))
+    for j in 1:length(αlist)
+        responses[j] = ptirflist[j][i]
+    end  
+    irfdistr[i,1] = mean(responses) 
+    irfdistr[i,2] = quantile(responses, 0.1)
+    irfdistr[i,3] = quantile(responses, 0.9)
+end 
+plot(irfdistr) 
