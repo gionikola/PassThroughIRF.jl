1153a783e013fc872f0027502cf7fd84af938ccb
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-11-03 11:09:40 -0700

    Modify ptirf() to faster method

--- src/ptirf.jl
+++ src/ptirf.jl
@@ -1,37 +1,8 @@
 function ptirf(response, medium, shock, periods, lagcoefmats, shockcoefmat) 
 
-    numvars = size(shockcoefmat)[1]
-    numlags = length(lagcoefmats)
-    paths = filterpaths(response, medium, numvars, periods)
-    responsevarnum = response 
-    response = []
-    #push!(response, shockcoefmat[responsevarnum, shock])
-    push!(response, 0.0)
-    for t in 2:(length(paths))
-        response_t = []
-        if t <= numlags 
-            for j in 0:(t-2)
-                if paths[t-j] == "empty"
-                    push!(response_t, 0.0)
-                else 
-                    for i in 1:length(paths[t-j])
-                        push!(response_t, pathintensity(paths[t-j][i], lagcoefmats[j+1], shockcoefmat, shock))
-                    end 
-                end 
-            end 
-        else 
-            for j in 0:(numlags-1)
-                if paths[t-j] == "empty"
-                    push!(response_t, 0.0)
-                else 
-                    for i in 1:length(paths[t-j])
-                        push!(response_t, pathintensity(paths[t-j][i], lagcoefmats[j+1], shockcoefmat, shock))
-                    end 
-                end 
-            end 
-        end 
-        push!(response, sum(response_t)) 
+    for i in 1:size(lagcoefmats)[1]
+        lagcoefmats[i][:,medium] = zeros(size(lagcoefmats[i])[1])
     end 
 
-    return response
+    return irf(response, shock, periods, lagcoefmats, shockcoefmat) 
 end 
