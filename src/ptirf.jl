028c94b45902e99d9fdcbde79ce7d09b0c36113b
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2022-11-03 13:48:01 -0700

    Fix ptirf() to avoid `lagcoefmats` getting mutated

--- src/ptirf.jl
+++ src/ptirf.jl
@@ -1,8 +1,11 @@
 function ptirf(response, medium, shock, periods, lagcoefmats, shockcoefmat) 
 
-    for i in 1:size(lagcoefmats)[1]
-        lagcoefmats[i][:,medium] = zeros(size(lagcoefmats[i])[1])
+    A = similar(lagcoefmats) 
+    for i in 1:size(A)[1]
+        A[i] = copy(lagcoefmats[i])
+        A[i][:,medium] = zeros(size(A[i])[1])
     end 
 
-    return irf(response, shock, periods, lagcoefmats, shockcoefmat) 
+    responses = irf(response, shock, periods, A, shockcoefmat) 
+    return responses 
 end 
